dbc1b7e325ae4eeb648a638221923f30309f8c0f
hatice860 <79165387+hatice860@users.noreply.github.com>
2021-05-10 22:18:51 +0300

    update

--- src/InstructorManager.java
+++ src/InstructorManager.java
@@ -1,5 +1,19 @@
 public class InstructorManager extends UserManager{
+ public void add(Instructor instructor){
+  System.out.println("Kullanıcı eklendi"+instructor.getFirstName());
+ }
 
+ public void update(Instructor instructor){
+  System.out.println("Kullanıcı güncellendi"+instructor.getFirstName());
+ }
+
+ public void delete(Instructor instructor){
+  System.out.println("Kullanıcı silindi"+instructor.getFirstName());
+ }
+
+ public void list(Instructor instructor){
+  System.out.println("Kullanıcı listelendi"+instructor.getFirstName());
+    }
  }
 
 
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
         User user = new User("Hatice", "Ay", "haticeay@email.com", "12345");
-        System.out.println(user.getFirstName());
-        Student student = new Student("", "", "", "", "12345");
 
+        Student student = new Student("", "", "", "", "12345");
+        Instructor instructor = new Instructor("", "", "", "");
         StudentManager studentManager = new StudentManager();
         studentManager.delete(student);
         UserManager userManager = new UserManager();
         userManager.add(user);
-
+        InstructorManager instructorManager = new InstructorManager();
+        instructorManager.list(instructor);
 
 
     }
