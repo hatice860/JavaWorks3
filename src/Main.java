681d0677227aab4d713920bd26434156a90b4d53
hatice860 <79165387+hatice860@users.noreply.github.com>
2021-05-10 22:11:48 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-	User user=new User("Hatice","Ay","haticeay@email.com","12345");
-	System.out.println(user.getFirstName());
+        User user = new User("Hatice", "Ay", "haticeay@email.com", "12345");
+        System.out.println(user.getFirstName());
+        Student student = new Student("", "", "", "", "12345");
 
-    Student student=new Student("","","","","12345");
-      System.out.println(student.getStudentNo());
+        StudentManager studentManager = new StudentManager();
+        studentManager.delete(student);
+        UserManager userManager = new UserManager();
+        userManager.add(user);
 
-     UserManager userManager=new UserManager();
-     System.out.println (user);
 
 
     }
